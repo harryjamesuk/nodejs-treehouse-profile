ce990be594efeaa23aeebc39d5223e5f7f0fe9ec
Harry James <harryjamesuk@users.noreply.github.com>
2016-08-21 23:54:04 +0100

    Connecting to profile
    200 OK - We're good to go :)
    
    Also set up printMessage() function.

---
+++ app.js
@@ -0,0 +1,13 @@
+var https = require("https");
+var username = "harryjamesuk";
+
+function printMessage(username, badgeCount, points) {
+  var message = username + " has " + badgeCount + " total badge(s) and " +
+  points + " points in JavaScript";
+  console.log(message);
+}
+
+var request = https.get("https://teamtreehouse.com/" + username + ".json",
+function(response) {
+  console.log(response.statusCode);
+});
