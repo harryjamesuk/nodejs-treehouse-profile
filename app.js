96e1430f0096e3996a78f91a5ba5871a5520356e
Harry James <harryjamesuk@users.noreply.github.com>
2016-08-25 17:58:01 +0100

    Created HTTP server
    Ready to rumble :)

---
+++ app.js
@@ -0,0 +1,22 @@
+// Create web server
+const http = require('http');
+
+const hostname = '127.0.0.1';
+const port = 3000;
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.write('This is before the end!\n');
+  res.end('Hello World\n');
+});
+
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
+
+// Handle HTTP route GET / and POST /
+
+// Handle HTTP route GET /username
+
+// Read files and merge in values.
